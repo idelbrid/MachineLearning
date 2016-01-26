f08e4a5dd7010a77a7274113bd09b7473bd03404
Ian Delbridge <idelbrid@sawteeth.csug.rochester.edu>
2016-01-26 17:10:11 -0500

    fleshed out fit and predict. Predict needs optimization (might change fit's storage style.

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -3,7 +3,6 @@ __author__ = 'idelbrid'
 import numpy as np
 import sys
 
-
 class naive_bayes:
     def __init__(self, alpha=1):
         self.alpha = alpha
@@ -11,13 +10,62 @@ class naive_bayes:
     def fit(self, X, y):
         """INPUT: training data matrix, traning data labels
         OUTPUT: weights of the model (ndarray)"""
+        #p(y=a | x1,...,xn) = p(x1,...,xn | y=a)*p(y=a) / p(x1,...xn)        
+        # p(x1,...,xn) same for all classes so ... don't need it
+        # p(y=1) = relative frequency of y=1
+        # p(x1,...,xn | y=1) = p(x1|y=1)...p(xn|y=1)
+        # p(y=1, x1,...,xn) goal 
+        # 
         self.X = X
         self.y = y
-        y_has_1 = (y == 1)
-        X_filtered = X[y_has_1]  # Not sure if this will run
-        cx = X_filtered.sum(axis=1)
-
-
+        unique_yvals, yval_cts = np.unique(y, return_counts=True)
+        # unique_yvals = unique_yvals_w_freq[:,0]
+        # yvals_cts = unique_yvals_w_freq[:,1]
+        has_yval_mask = [y == yval for yval in unique_yvals]
+        # y_has_yval = [y[has_yval_mask[yval]] for yval in unique_yvals]
+        # y = np.concatenate(y_has_0, y_has_1, axis=1)
+        # x_cts = [np.ndarray((1,1)) for yval in unique_yvals]
+        X_has_yval = [X[has_yval_mask[yval_indx]] for yval_indx in range(len(unique_yvals))]
+        xval_cts = [dict() for x in unique_yvals]
+        for i, yval in enumerate(unique_yvals):
+            frequencies = np.array([np.unique(x_column, return_counts=True) for 
+                                x_column in X_has_yval[i].T]) #freq for each attribute
+            xval_cts[i] = [{att_values[indx]: freqs[indx] for indx in range(len(att_values))} for 
+                                 (att_values, freqs) in frequencies] # stats for each attribute
+        self.num_atts = len(X[0])
+        self.num_recrds = len([X])
+        self.yvals = unique_yvals
+        self.num_labels = len(self.yvals)
+        self.ycts = yval_cts
+        self.xval_cts = xval_cts
+         
+    def predict(self, X):
+        scores = np.zeros((len(X), self.num_labels))
+        predictions = np.zeros((len(X)))
+        for x_indx, record in enumerate(X):
+            max_score, best_y = (0, -1)
+            for indx, yval in enumerate(self.yvals):
+                xval_cts = self.xval_cts[indx]
+                cursum = 0
+                for att, val in enumerate(record):
+                    try:
+                        cursum += np.log((xval_cts[att][val] + self.alpha)/(
+                            self.ycts[indx]+self.num_labels * self.alpha))
+                    except KeyError:
+                        cursum += np.log(self.alpha * 1.0 / (self.ycts[indx] + 
+                                         self.num_labels * self.alpha))
+                        # never seen this y with this attribute's value - 0 ct
+                        # print x_indx, record, indx, yval, att, val
+                s = cursum * self.ycts[indx]
+                scores[x_indx, indx] = s
+                if s > max_score:
+                    # best_y_indx = indx
+                    # best_y = yval
+                    predictions[x_indx] = yval
+        self.scores = scores
+        self.predictions = predictions
+        return predictions
+        
 def read_data(file_str, num_feats):
     """ INPUT: string denoting the file containing the dataset
         OUTPUT: matrix of the data """
@@ -58,8 +106,8 @@ if __name__ == "__main__":
     test_data, test_labels = read_data(testfile, num_feats)
 
     model = naive_bayes(alpha=1)
-    model.fit(np.asmatrix(train_data), train_labels)
-    predictions = model.predict(np.asmatrix(test_data))
+    model.fit(train_data, train_labels)
+    predictions = model.predict(test_data)
 
     accuracy, num_right, total_pts = evaluate_accuracy(test_labels, predictions)
 
