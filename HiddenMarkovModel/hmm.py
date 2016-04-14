aef549d0e3c497a5fb466bb55b7f4174ec32b61c
Ian Delbridge <idelbrid@u.rochester.edu>
2016-04-14 13:58:02 -0400

    Fixed all of the index problems, I think, by adjusting to the book's notation. Wrote pred (correctly) and started best of ten.

--- HiddenMarkovModel/hmm.py
+++ HiddenMarkovModel/hmm.py
@@ -14,6 +14,7 @@ class HMM:
         self.X = None
         self.max_iter = max_iter
         self.random_seed = init_seed
+        np.random.seed(self.random_seed)
         self.T = None
         self.A = None
         self.B = None
@@ -21,50 +22,73 @@ class HMM:
         self.mu = None
         self.sigma = None
 
-    def forward_backward(self):  # todo: actually test, speed up, alter for initial states
+    def forward_backward(self, X=None):
+
         T = self.T
-        alpha = np.zeros((T, self.num_gaussians))
-        beta = np.zeros((T, self.num_gaussians))
         A = self.A
-        B = self.B
+        if X is None:
+            B = self.B  # training
+        else:
+            B = np.zeros((T, self.num_gaussians))  # testing
+            for k in range(self.num_gaussians):
+                B[:, k] = normal(self.mu[k], self.sigma[k]).pdf(X)
         # alpha[-1, self.num_gaussians] = 1
         # alpha[-1, :self.num_gaussians] = 0
-        for t in range(0, T):  # Standard calculation
-            if t == 0:
-                for j in range(self.num_gaussians):
-                    alpha[0, j] = self.pi[j] * self.B[0, j]
-            else:
-                for j in range(self.num_gaussians):
-                    for i in range(self.num_gaussians):
-                        alpha[t, j] += alpha[t-1, i] * A[j, i] * B[t+1, j]
 
-        for t in range(T-1, -1, -1):  # off by one?
-            if t == T-1:
-                for i in range(self.num_gaussians):
-                    beta[T-1, i] = 1
-            else:
-                for i in range(self.num_gaussians):
-                    for j in range(self.num_gaussians):
-                        beta[t, i] += beta[t+1, j] * A[j, i] * B[t+1, j]
+        c = np.zeros(T)
+        # alpha = np.zeros((T, self.num_gaussians))
+        # beta = np.zeros((T, self.num_gaussians))
+        # for t in range(0, T):  # Standard calculation
+        #     if t == 0:
+        #         for j in range(self.num_gaussians):
+        #             alpha[0, j] = self.pi[j] * self.B[0, j]
+        #
+        #     else:
+        #         for j in range(self.num_gaussians):
+        #             for i in range(self.num_gaussians):
+        #                 alpha[t, j] += alpha[t-1, i] * A[i, j] * B[t, j]
+        #     c[t] = alpha[t, :].sum()
+        #     alpha[t, :] /= c[t]  # Normalize to alpha hat as in Bishop
+        #
+        # for t in range(T-1, -1, -1):
+        #     if t == T-1:
+        #         for i in range(self.num_gaussians):
+        #             beta[T-1, i] = 1
+        #     else:
+        #         for i in range(self.num_gaussians):
+        #             for j in range(self.num_gaussians):
+        #                 beta[t, i] += beta[t+1, j] * A[i, j] * B[t+1, j]
+        #         beta[t, :] /= c[t+1]
 
-        a = np.zeros(T, self.num_gaussians)
-        b = np.zeros(T, self.num_gaussians)
-        for t in range(0, T):  # Faster? Works? Todo verify if this is an alternative
-            a[t, :] = np.dot(A, a[t-1, :]) * B[t, :]
+        # return alpha, beta
+        # a == alpha
+        # b == beta
 
-        for t in range(T-1, 0, -1):
-            b[t, :] = np.dot(A, b[t+1, :]) * B[t+1, :]
+        a = np.zeros((T, self.num_gaussians))
+        b = np.zeros((T, self.num_gaussians))
+        for j in range(self.num_gaussians):
+            a[0, j] = self.pi[j] * self.B[0, j]
+        c[0] = a[0, :].sum()
+        a[0, :] /= c[0]
 
-        # a == alpha?
-        # b == beta?
-        return alpha[:-1, :-1], beta[:-1, :-1]
+        for t in range(1, T):  # The same as above, but vectorized
+            a[t, :] = np.dot(A.T, a[t-1, :]) * B[t, :]
+            c[t] = a[t, :].sum()
+            a[t, :] /= c[t]
+
+        for j in range(self.num_gaussians):
+            b[T-1, j] = 1
+        for t in range(T-2, -1, -1):
+            b[t, :] = np.dot(A, (b[t+1, :]) * B[t+1, :] )
+            b[t, :] /= c[t+1]
+
+        return a, b, c
 
     def fit(self, X):
         self.X = X
         matX = np.asmatrix(self.X)
         self.T = X.shape[0]
         self.ndim = X.shape[1]
-        np.random.seed(self.random_seed)
 
         # initialization schemes
         if self.init_method == 'random':
@@ -94,39 +118,53 @@ class HMM:
         for iter in range(self.max_iter):
 
             # E step
-            phat = np.zeros((self.T, self.num_gaussians))
-            alpha, beta = self.forward_backward()
+            alpha, beta, c = self.forward_backward()  # c is scaling factors
             ect = np.zeros((self.num_gaussians, self.num_gaussians))
 
-            for t in range(self.T):
+            for i in range(self.num_gaussians):
+                gamma[0, i] = alpha[0, i] * beta[0, i]
+            for t in range(1, self.T):  # skip 0 - covered by pi
                 for i in range(self.num_gaussians):
-                    gamma[t, i] = alpha[t, i] * beta[t, i] / NORMALIZATION  # TODO
+                    gamma[t, i] = alpha[t, i] * beta[t, i]  # normalize below
                     for j in range(self.num_gaussians):
-                        ksi[i, j, t] = alpha[j, t-1] * beta[i, t] * self.A[i, j] * self.B[t, i] / NORMAL
+                        ksi[j, i, t] = alpha[t-1, j] * beta[t, i] * self.A[j, i] * self.B[t, i] / c[t] # may still have i, j backwards
+                        ect[j, i] += ksi[j, i, t]  # expected count of zn =j and zn-1 =i is sum over t of p(z_{n-1}=j, z_n=i)
 
             # M step
-            self.A = ect / ect.sum(axis=1)  # check this is the right axis
-            self.pi = gamma[1, :] / gamma[1, :].sum()
+            self.A = ect / ect.sum(axis=1)  # appears to be right axis... sum over the posterior, i.e. axis 1 (rows)
+            self.pi = gamma[0, :] / gamma[0, :].sum()  # Checks out with maths
             for k in range(self.num_gaussians):
-                norm = phat[:, k].sum()
-                self.mu[k] = np.dot(phat[:, k], X) / norm
-                intermed = np.multiply(phat[:, k], (matX - mu[k]).T).T
-                self.sigma[k] = np.dot(intermed.T, (matX - mu[k])) / norm
-                self.B[:, k] = normal(self.mu[k], self.sigma[k]).pdf(X)
+                norm = gamma[:, k].sum()
+                self.mu[k] = np.dot(gamma[:, k], X) / norm  # looks right
+                intermed = np.multiply(gamma[:, k], (matX - self.mu[k]).T).T
+                self.sigma[k] = np.dot(intermed.T, (matX - self.mu[k])) / norm  # since taken from GAUSS MIX, correct
+                self.B[:, k] = normal(self.mu[k], self.sigma[k]).pdf(X)  # recalculating the table of densities
 
+    def pred(self, test_data):  # todo
+        if len(test_data) != self.T:
+            raise ValueError("Testing data does not match the length of the training data.")
+        alpha, beta, c = self.forward_backward(test_data)
 
-
-    def pred(self, test_data):  # use learned gaussians to predict most likely distribution and total likelihood of the pt
-        num_pts = len(test_data)
-        p = np.zeros((num_pts, self.num_gaussians), dtype=np.float64)
-
+        gamma = alpha * beta
+        pred_labels = gamma.argmax(axis=1)  # predicted labels, i.e. choices of latent variable z
+        testB = np.zeros((self.T, self.num_gaussians))
         for k in range(self.num_gaussians):
-            normal_var = normal(mean=self.mu[k], cov=self.sigma[k])
-            p[:, k] = self.lmbda[k] * normal_var.pdf(test_data)
-        pred_labels = p.argmax(axis=1)
-        likelihood = p.sum(axis=1)
-        return pred_labels, likelihood
+            testB[:, k] = normal(self.mu[k], self.sigma[k]).pdf(test_data)
 
+        ksi = np.zeros((self.num_gaussians, self.num_gaussians, self.T))
+        for t in range(1, self.T):  # skip 0 - covered by pi
+            for i in range(self.num_gaussians):
+                for j in range(self.num_gaussians):
+                    ksi[j, i, t] = alpha[t-1, j] * beta[t, i] * self.A[j, i] * testB[t, i] / c[t] # may still have i, j backwards
+
+        log_likelihood = (gamma[0, :] * self.pi).sum() + (ksi.sum(axis=2) * np.log(self.A)).sum() + (gamma * testB).sum()
+        return pred_labels, log_likelihood
+
+
+def best_of_10(X, max_iter):
+    models = list()
+    for i in range(10):
+        m = HMM(max_iter=max_iter)
 
 def read_data(file_str, num_feats):
     """ INPUT: string denoting the file containing the dataset, number of features
@@ -175,13 +213,11 @@ if __name__ == "__main__":
 
     model = HMM(30)
     model.fit(train_data)  # fitting
-    train_labels, train_likelihoods = model.pred(train_data)
-    test_labels, test_likelihoods = model.pred(test_data)
+    train_labels, log_likelihood = model.pred(train_data)
+    # test_labels, test_likelihoods = model.pred(test_data)
 
-    log_likelihood = np.log(test_likelihoods).sum()  # likelihoods returned are for each point
 
-    print np.log(train_likelihoods).sum(), "log likelihood on first 90% of data"
-    print log_likelihood, "log likelihood on last 10% of data"
+    print log_likelihood, "log likelihood on first 90% of data"
 
     # with open('label_logs_rand.csv', 'w+') as f:
     #     f.write("dim1,dim2,label,likelihood\n")
